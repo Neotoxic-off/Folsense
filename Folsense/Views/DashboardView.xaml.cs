4a8ddc96af610af68ec659bd17cdb9879eefb4d0
neo <neotoxic.off@gmail.com>
2024-04-16 16:20:58 +0200

    Add animation
    chat gtped might have to fix as usual

--- Folsense/Views/DashboardView.xaml.cs
+++ Folsense/Views/DashboardView.xaml.cs
@@ -23,6 +23,56 @@ namespace Folsense.Views
         public DashboardView()
         {
             InitializeComponent();
+
+            Database.ItemContainerGenerator.ItemsChanged += ItemContainerGenerator_ItemsChanged;
+        }
+
+        private void ItemContainerGenerator_ItemsChanged(object sender, ItemsChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                // Get the newly added item container
+                var newItemContainer = Database.ItemContainerGenerator.ContainerFromIndex(e.Position.Index) as UIElement;
+
+                // Apply animation to the newly added item container
+                if (newItemContainer != null)
+                {
+                    var storyboard = new Storyboard();
+                    var fadeInAnimation = new DoubleAnimation
+                    {
+                        From = 0,
+                        To = 1,
+                        Duration = TimeSpan.FromSeconds(0.5)
+                    };
+                    Storyboard.SetTargetProperty(fadeInAnimation, new PropertyPath(OpacityProperty));
+                    storyboard.Children.Add(fadeInAnimation);
+                    newItemContainer.BeginAnimation(OpacityProperty, fadeInAnimation);
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                var removedItemContainer = Database.ItemContainerGenerator.ContainerFromIndex(e.Position.Index) as UIElement;
+
+                // Apply animation to the removed item container
+                if (removedItemContainer != null)
+                {
+                    var fadeOutStoryboard = new Storyboard();
+                    var fadeOutAnimation = new DoubleAnimation
+                    {
+                        From = 1,
+                        To = 0,
+                        Duration = TimeSpan.FromSeconds(0.5)
+                    };
+                    fadeOutAnimation.Completed += (s, _) =>
+                    {
+                        // After animation completes, remove the item from UI
+                        Database.Items.Remove(Database.Items[e.Position.Index]);
+                    };
+                    Storyboard.SetTargetProperty(fadeOutAnimation, new PropertyPath(OpacityProperty));
+                    fadeOutStoryboard.Children.Add(fadeOutAnimation);
+                    removedItemContainer.BeginAnimation(OpacityProperty, fadeOutAnimation);
+                }
+            }
         }
     }
 }
